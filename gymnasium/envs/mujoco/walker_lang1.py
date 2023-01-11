8fcb0311890b104db75f33d0190846c7e649c123
Antoine de Comite <53222045+decomiteA@users.noreply.github.com>
2023-01-11 14:34:54 -0500

    Update walker_lang1.py

--- gymnasium/envs/mujoco/walker_lang1.py
+++ gymnasium/envs/mujoco/walker_lang1.py
@@ -41,3 +41,25 @@ class WalkerLang1Env(MuJocoPyEnv, utils.EzPickle):
     if self.render_mode =="human":
       self.render()
     return ob, reward, terminated, False, {}
+  
+  def _get_obs(self):
+    return = np.concatenate(
+      [self.sim.data.qpos.flat[1:], np.clip(self.sim.data.qvel.flat, -10, 10)]
+    )
+  
+  def reset_model(self):
+    qpos = self.init_qpos + self.np_random.uniform(
+      low = -0.005, high = 0.005, size=self.model.nq
+    )
+    qvel = self.init_qvel + self.np_random.uniform(
+      low = -0.005, high = 0.005, size=self.model.nv
+    )
+    self.set_state(qpos,qvel)
+    return self._get_obs()
+  
+  def viewer_setup(self):
+    assert self.viewer is not None
+    self.viewer.cam.trackbodyid = 2
+    self.viewer.cam.distance = self.model.stat.extent * 0.75
+    self.viewer.cam.lookat[2] = 1.15
+    self.viewer.cam.elevation = -20
